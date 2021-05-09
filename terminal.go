f6ad0e712e8fd63f15d681996974e9c4c6dd712b
Ridho Pratama <ridho.pratama@go-jek.com>
2021-05-09 13:06:53 +0700

    Added number and numbers terminal

--- terminal.go
+++ terminal.go
@@ -45,7 +45,7 @@ func Alphas() Parser {
 			}
 
 			builder := strings.Builder{}
-			for ps.idx >= len(ps.input) {
+			for ps.idx < len(ps.input) {
 				head := ps.input[ps.idx]
 				if !isByteAlpha(head) {
 					break
@@ -64,8 +64,71 @@ func Alphas() Parser {
 	}
 }
 
+// Number takes a single numerical `/[0-9]/`` character and return it in a string.
+func Number() Parser {
+	return Parser{
+		Func: func(ps ParserState) ParserState {
+			if ps.err != nil {
+				return ps
+			}
+
+			if ps.outOfBound() {
+				ps.err = fmt.Errorf("expected numerical but found end of input")
+				return ps
+			}
+
+			head := ps.input[ps.idx]
+			if isByteNumber(head) {
+				ps.result = ps.input[ps.idx : ps.idx+1]
+				ps.idx += 1
+				return ps
+			}
+
+			ps.err = fmt.Errorf("expected numerical but found \"%.16s\"", ps.input[ps.idx:])
+			return ps
+		},
+	}
+}
+
+// Numbers takes more than one numerical `/[0-9]+/` characters and return it in a string.
+func Numbers() Parser {
+	return Parser{
+		Func: func(ps ParserState) ParserState {
+			if ps.err != nil {
+				return ps
+			}
+
+			if ps.outOfBound() {
+				ps.err = fmt.Errorf("expected numerical but found end of input")
+				return ps
+			}
+
+			builder := strings.Builder{}
+			for ps.idx < len(ps.input) {
+				head := ps.input[ps.idx]
+				if !isByteNumber(head) {
+					break
+				}
+
+				builder.WriteByte(head)
+				ps.idx += 1
+			}
+
+			ps.result = builder.String()
+			if ps.result == "" {
+				ps.err = fmt.Errorf("expected numerical but found \"%.16s\"", ps.input[ps.idx:])
+			}
+			return ps
+		},
+	}
+}
+
 func isByteAlpha(b byte) bool {
 	insideLower := ('a' <= b) && (b <= 'z')
 	insideUpper := ('A' <= b) && (b <= 'Z')
 	return insideLower || insideUpper
+}
+
+func isByteNumber(b byte) bool {
+	return ('0' <= b) && (b <= '9')
 }
--- terminal_test.go
+++ terminal_test.go
@@ -79,3 +79,77 @@ func TestAlphas(t *testing.T) {
 		})
 	}
 }
+
+func TestNumber(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       string
+		want        string
+		shouldError bool
+	}{
+		{
+			name:  "success",
+			input: "123",
+			want:  "1",
+		},
+		{
+			name:        "fail",
+			input:       "hello",
+			shouldError: true,
+		},
+		{
+			name:        "empty",
+			input:       "",
+			shouldError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := Number().Run(tt.input)
+
+			if !tt.shouldError {
+				assert.Equal(t, tt.want, actual.Result())
+				assert.Nil(t, actual.Error())
+			} else {
+				assert.Error(t, actual.Error())
+			}
+		})
+	}
+}
+
+func TestNumbers(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       string
+		want        string
+		shouldError bool
+	}{
+		{
+			name:  "success",
+			input: "123 456",
+			want:  "123",
+		},
+		{
+			name:  "success 2",
+			input: "123",
+			want:  "123",
+		},
+		{
+			name:        "minimal one",
+			input:       "a",
+			shouldError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := Numbers().Run(tt.input)
+
+			if !tt.shouldError {
+				assert.Equal(t, tt.want, actual.Result())
+				assert.Nil(t, actual.Error())
+			} else {
+				assert.Error(t, actual.Error())
+			}
+		})
+	}
+}
