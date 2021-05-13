582536ed53cf4365ea1188c7c90c1955d9434d56
Ridho Pratama <ridho.pratama@go-jek.com>
2021-05-13 16:00:42 +0700

    Added more util parser

--- util.go
+++ util.go
@@ -1,19 +1,19 @@
 package pegchamp
 
-// ParserPointer takes a pointer to a Parser, which returns a lazy Parser
-// which only dereference the pointer in runtime.
-// Makes recursive parser and breaks initialization cycle.
-func ParserPointer(p *Parser) Parser {
-	return Parser{
-		parserFunc: func(ps ParserState) ParserState {
-			return (*p).Parse(ps)
-		},
-	}
-}
-
 func IgnoreLeadingWhitespace(p Parser) Parser {
 	return TakeSecond(
 		OptionalWhitespaces(),
 		p,
 	)
+}
+
+// ParserFunc takes a `func() Parser`
+// which called in runtime.
+// Makes recursive parser and breaks initialization cycle.
+func ParserFunc(p func() Parser) Parser {
+	return Parser{
+		parserFunc: func(ps ParserState) ParserState {
+			return p().Parse(ps)
+		},
+	}
 }
